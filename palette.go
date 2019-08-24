0cc88fce02b746987af9406992debbbb0341d602
Caleb Thorsteinson <caleb@thorsteinson.io>
2019-08-24 14:37:41 -0700

    Implements Grayscale palette generator
    Palette creators are separate from the palettes themselves. Since
    there's quite a bit of repetetive math involved in generating the
    palette, we should aim to do that only once, and then use the palette
    as a lookup function.

--- palette.go
+++ palette.go
@@ -2,13 +2,43 @@ package fractal
 
 import (
 	"image/color"
+	"math"
 )
 
 // The simplest possible coloring function. It colors points in the
 // Mandelbrot set black, and those not in the set white
-func BlackWhite (c IterationCount) color.Color {
+func BlackWhite(c IterationCount) color.Color {
 	if c.Count < 0 {
 		return color.Black
 	}
 	return color.White
+}
+
+// Generates a grayscale palette to a coloring spectrum which is
+// mirrored. Max is the number of maximum interations, for selecting
+// the number in the cycle. Finally, resolution is the number of
+// grayscale colors to pick for generating the palette.
+func Grayscale(resolution int, percentGray float64) []color.Color {
+	startingLuminosity := 1.0
+	var midpointLuminosity = percentGray * float64(math.MaxUint16)
+	deltaLum := (startingLuminosity - midpointLuminosity) / float64(resolution)
+
+	// Construct palette in terms of normalized luminosities
+	luminosities := []float64{}
+	for i := 0; i <= resolution; i++ {
+		lum := startingLuminosity - (float64(i) * deltaLum)
+		luminosities = append(luminosities, lum)
+	}
+	// Mirror the palette
+	for i := len(luminosities) - 2; i >= 0; i-- {
+		luminosities = append(luminosities, luminosities[i])
+	}
+
+	// Convert Palette into actual list of grayscale colors
+	palette := make([]color.Color, len(luminosities))
+	for i, lum := range luminosities {
+		palette[i] = color.Gray16{uint16(lum * float64(math.MaxUint16))}
+	}
+
+	return palette
 }
--- palette_test.go
+++ palette_test.go
@@ -15,3 +15,28 @@ func TestBlackWhite(t *testing.T) {
 		t.Error("Failed to color result white")
 	}
 }
+
+// Tests that grayscale generates a palette as expected
+func TestGrayscale(t *testing.T) {
+
+	res := 5
+	// Construct a grayscale palette that varies between complete
+	// white and black, with 5 colors mirrored
+	grays := Grayscale(res, 0)
+
+	if len(grays) != res*2 + 1 {
+		t.Errorf("Incorrect number of colors found in palette. Expect %v; Found %v", res*2 + 1, len(grays))
+	}
+
+	if grays[0] != color.White {
+		t.Errorf("Starting color is not white. Found %v", grays[0])
+	}
+
+	if grays[len(grays) - 1] != color.White {
+		t.Errorf("Ending color is not white. Found %v", grays[len(grays) -1 ])
+	}
+
+	if grays[res] != color.Black {
+		t.Errorf("Middle color is not black. Found %v", grays[res])
+	}
+}
